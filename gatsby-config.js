ba4e46e573077e56b6a4c0792e7d412d3d784223
Snigur69 <asnigur69@gmail.com>
2020-12-12 22:22:51 +0200

    itegrated wordpress to gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,14 @@
 module.exports = {
     plugins: [
-
+        {
+            resolve: "gatsby-source-wordpress",
+            options: {
+              baseUrl: "wordpress.wg-an.site/",
+              protocol: "http",
+              restApiRoutePrefix: "wp-json",
+              hostingWPCOM: false,
+              useACF: true,
+            },
+          },
     ]
 }
